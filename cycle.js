52c209d37c099cc6d868d1678ccdf25f98f1fe9c
Benjamin Doherty <bjd@pobox.com>
2011-02-14 13:11:36 -0500

    code comments in javascript behavior.

--- cycle.js
+++ cycle.js
@@ -1,7 +1,11 @@
 (function ($) {
 
   Drupal.behaviors.cycle = {
+
     attach: function (context, settings) {
+
+      // Settings object properties are element IDs and the values are settings
+      // for jQuery Cycle.
       $.each(settings.cycle, function (id) {
         $('#' + id).cycle(this);
       });
